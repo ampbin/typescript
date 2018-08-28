e7efebff96e44c7ef24dd486ab9792c3a7633a88
Levi Durfee <levi.durfee@gmail.com>
2018-08-28 19:22:51 -0400

    Add code to Database save and load methods

--- src/Database.ts
+++ src/Database.ts
@@ -7,11 +7,31 @@ export class Database {
         this.db = db;
     }
     
-    save() {
-        
+    save(userId: string, editor: CodeMirror.Editor) {
+        this.db.collection('bins').add({
+            user: userId,
+            amphtml: editor.getValue(),
+            saved: new Date().getTime()
+        }).then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            // Update URL with hash
+            // Show toast notification
+        }).catch((error) => {
+            console.error("Error adding document: ", error);
+        })
+        ;
     }
     
-    load() {
+    load(binId: string, editor: CodeMirror.Editor) {
+        let docRef = this.db.collection('bins').doc(binId.replace("#", ""));
         
+        docRef.get().then((doc) => {
+            if(doc.exists) {
+                return editor.setValue(doc.data().amphtml);
+            }
+        }).catch((error) => {
+            console.error("Error reading document: ", error);
+        })
+        ;
     }
 }
