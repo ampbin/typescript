7cb76426916ad428542302a88c94a952537b6e7a
Levi Durfee <levi.durfee@gmail.com>
2018-09-06 13:30:16 -0400

    Add GoogleAuthProvider and add event handler

--- src/app.ts
+++ src/app.ts
@@ -10,25 +10,28 @@ import { getEl, getHash } from './Helpers';
 function ampbin(ampbinConfig: any) {
     // Load firebase
     firebase.initializeApp(ampbinConfig.firebase);
-    
+
     // Load core components
-    let auth = new Auth(firebase.auth());
+    let auth = new Auth(firebase.auth(), {
+        google: new firebase.auth.GoogleAuthProvider()
+    });
     let db = new Database(firebase.firestore(), {timestampsInSnapshots: true});
     let editor = new Editor(ampbinConfig.editor);
     let events = new Events(auth, db, editor);
-    
+
     // Not logged in with an account, log them in anonymously
     if(firebase.auth().currentUser == null) {
         auth.loginAnonymously();
     }
-    
+
     // Listen for auth change
     events.authChange();
-    
+
     // Event handlers
     // # Save bin
     getEl(ampbinConfig.save).onclick = () => events.save();
-    
+    getEl(ampbinConfig.signin).onclick = () => events.loginWithGoogle();
+
     // Loader
     // # Load bin if there is a hash in the URL
     (getHash().length > 0) ? db.load(getHash(), editor) : '';
