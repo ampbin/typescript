cd077fb00dd13f5037e3177dd562395e961f0439
Levi Durfee <levi.durfee@gmail.com>
2018-09-06 13:30:02 -0400

    Handle click and call Auth login method

--- src/Events.ts
+++ src/Events.ts
@@ -6,17 +6,21 @@ export class Events {
     private auth: Auth;
     private db: Database;
     private editor: Editor;
-    
+
     constructor(auth: Auth, db: Database, editor: Editor) {
         this.db = db;
         this.editor = editor;
         this.auth = auth;
     }
-    
+
+    loginWithGoogle() {
+        this.auth.login();
+    }
+
     save() {
         this.db.save(this.auth.getUserId(), this.editor);
     }
-    
+
     authChange() {
         this.auth.getAuth().onAuthStateChanged(function(user) {
           if (user) {
