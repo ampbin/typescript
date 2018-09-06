360ff5098323a34d07eb2794634e9ff03a57f222
Levi Durfee <levi.durfee@gmail.com>
2018-09-06 13:29:51 -0400

    Add login using google method

--- src/Auth.ts
+++ src/Auth.ts
@@ -1,32 +1,39 @@
 export class Auth {
     private auth: firebase.auth.Auth;
-    
-    constructor(auth: firebase.auth.Auth) {
+    private googleProvider: firebase.auth.AuthProvider;
+    private user: any;
+
+    constructor(auth: firebase.auth.Auth, providers: any) {
         this.auth = auth;
+        this.googleProvider = providers.google;
     }
-    
+
     login() {
-        
+        this.auth.signInWithPopup(this.googleProvider).then((result) => {
+            this.user = result.user;
+        }).catch((err) => {
+            console.error(err);
+        });
     }
-    
+
     loginAnonymously() {
         this.auth.signInAnonymously().catch(function(error) {
             console.error(error);
         });
     }
-    
+
     logout() {
-        
+
     }
-    
+
     getAuth() {
         return this.auth;
     }
-    
+
     getCurrentUser() {
         return this.auth.currentUser;
     }
-    
+
     getUserId() {
         return this.auth.currentUser.uid;
     }
